824f703d086eff3537ded847b99a156f62779971
vvhoang <vvhoang@mgmgerrit.mgm-tp.com>
2019-09-20 13:04:56 +0700

    Updates

---
+++ precache-manifest.b04c46f2b2973f50d67a5a3881772c22.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89bce8f371879f50534cc0d6b11b11ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0df02b98d871b6cb05e4",
+    "url": "/static/css/main.9bad68fe.chunk.css"
+  },
+  {
+    "revision": "c2c609d5401dd2c0fb51",
+    "url": "/static/js/2.b075873a.chunk.js"
+  },
+  {
+    "revision": "0df02b98d871b6cb05e4",
+    "url": "/static/js/main.9055de35.chunk.js"
+  },
+  {
+    "revision": "cbe76a040c1265bbdd15",
+    "url": "/static/js/runtime~main.09d41f16.js"
+  }
+]);
